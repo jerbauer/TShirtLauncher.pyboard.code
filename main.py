c922074292329ff74b003fff288c81f73696e9d3
jerbauer <48531469+jerbauer@users.noreply.github.com>
2019-03-15 17:44:15 -0500

    Add files via upload
    internal LED and FB. FB depressed for 3 seconds -> FB changes from red to green and internal LED (2) turns on. When P_in.value() == 1, count restarts, internal LED off and FB back to red.

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from pyb import Pin
 #initialize pin X2
 P_in = Pin('X2', Pin.IN, Pin.PULL_UP)
 
+#intialize output for FB lights pin 
+pinrled = Pin('X4', Pin.OUT_PP)
+pingled = Pin('X6', Pin.OUT_PP)
 
 #intialize timer 2 (for front button)
 micros = pyb.Timer(2, prescaler=83, period=0x3fffffff)
@@ -15,18 +18,24 @@ micros = pyb.Timer(2, prescaler=83, period=0x3fffffff)
 #assign variable equal to 3 seconds (required depression time for FB) at 1MHz
 t_min = 3000000
 
-
+# pinrled.low()
+# pingled.high()
 while True:
     if P_in.value() == 1:
         count = micros.counter(0)
-        pyb.LED(3).off()
+        pyb.LED(2).off()
+        pinrled.high()
+        pingled.low()
     elif P_in.value() == 0:
         count = micros.counter()
         while P_in.value() == 0 and count >= t_min:
-            pyb.LED(3).on()
+            pinrled.low()
+            pyb.LED(2).on()
+            pingled.high()
     else: 
-        pyb.LED(3).off()
-
+        pyb.LED(2).off()
+        pinrled.high()
+        pingled.low()
 
 
 
