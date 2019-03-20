7f02e96081080e67709e3af7b563478d8ab6be15
jerbauer <48531469+jerbauer@users.noreply.github.com>
2019-03-20 14:13:49 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 # main.py -- put your code here!
 
-
 import pyb
 from pyb import Pin
+from pyb import Switch
+# from encoder import Encoder
 
-
-#initialize pin X2
+##INITIALIZING EXTERNAL COMPONENTS
+#initialize pin X2 for FB
 P_in = Pin('X2', Pin.IN, Pin.PULL_UP)
 
 #intialize output for FB lights pin 
 pinrled = Pin('X4', Pin.OUT_PP)
 pingled = Pin('X6', Pin.OUT_PP)
 
+
+## INITIALIZING BOARD COMPONENTS
 #intialize timer 2 (for front button)
 micros = pyb.Timer(2, prescaler=83, period=0x3fffffff)
 
+#initialize switch 'USR'
+sw = Switch()
+
+## INTIALIZING VARIABLES
 #assign variable equal to 3 seconds (required depression time for FB) at 1MHz
 t_min = 3000000
 
-# pinrled.low()
-# pingled.high()
+
 while True:
     if P_in.value() == 1:
         count = micros.counter(0)
         pyb.LED(2).off()
+        pyb.LED(3).off()
         pinrled.high()
         pingled.low()
     elif P_in.value() == 0:
@@ -32,11 +39,13 @@ while True:
             pinrled.low()
             pyb.LED(2).on()
             pingled.high()
+            if sw.value() == True:
+                pyb.LED(3).on()
+            else: 
+                pyb.LED(3).off()
     else: 
         pyb.LED(2).off()
+        pyb.LED(3).off()
         pinrled.high()
         pingled.low()
-
-
-
 
