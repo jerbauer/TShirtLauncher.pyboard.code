00d236e096deddd9653be57aad72dd404c6e1fb2
Thomas Reynolds <38272584+wetstreak@users.noreply.github.com>
2019-03-20 15:25:55 -0500

    Motor Calibration for Victor SPX
    Total bandwidth is 4ms, neutral is 1.5ms, +- .5ms for forward reverse. Hold USR for neutral.

---
+++ motor_calibration.py
@@ -0,0 +1,80 @@
+# # main.py -- put your code here!
+
+# import pyb
+# from pyb import Pin
+# from pyb import Switch
+# # from encoder import Encoder
+
+# ##INITIALIZING EXTERNAL COMPONENTS
+# #initialize pin X2 for FB
+# P_in = Pin('X2', Pin.IN, Pin.PULL_UP)
+
+# #intialize output for FB lights pin 
+# pinrled = Pin('X4', Pin.OUT_PP)
+# pingled = Pin('X6', Pin.OUT_PP)
+
+
+# ## INITIALIZING BOARD COMPONENTS
+# #intialize timer 2 (for front button)
+# micros = pyb.Timer(2, prescaler=83, period=0x3fffffff)
+
+# #initialize switch 'USR'
+# #sw = Switch()
+
+# ## INTIALIZING VARIABLES
+# #assign variable equal to 3 seconds (required depression time for FB) at 1MHz
+# t_min = 3000000
+
+
+# while True:
+#     if P_in.value() == 1:
+#         count = micros.counter(0)
+#         pyb.LED(2).off()
+#         pyb.LED(3).off()
+#         pinrled.high()
+#         pingled.low()
+#     elif P_in.value() == 0:
+#         count = micros.counter()
+#         while P_in.value() == 0 and count >= t_min:
+#             pinrled.low()
+#             pyb.LED(2).on()
+#             pingled.high()
+#             if sw.value() == True:
+#                 pyb.LED(3).on()
+#             else: 
+#                 pyb.LED(3).off()
+#     else: 
+#         pyb.LED(2).off()
+#         pyb.LED(3).off()
+#         pinrled.high()
+#         pingled.low()
+
+import pyb
+import time
+from pyb import Pin, Timer, Switch
+
+
+pch = Pin('X1')
+sw = Switch()
+tim = pyb.Timer(2, freq = 250)
+
+ch = tim.channel(1, Timer.PWM, pin=pch)
+
+p=25
+q = 50
+while True:
+    while sw.value()==0:
+        if p <= 50:
+            p += 1
+            ch.pulse_width_percent(p)
+            time.sleep_ms(20)
+        elif q > 25:
+            q -= 1
+            ch.pulse_width_percent(q)
+            time.sleep_ms(20)
+        else:
+            p = 25
+            q = 50
+    else:
+        ch.pulse_width_percent(37.5)  
+ 
